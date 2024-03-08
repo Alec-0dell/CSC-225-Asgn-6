ae8045313bb4b7271aa41205c2cd67885e4ae7b5
A1ec-0dell <aqodell@calpoly.edu>
2024-03-07 21:37:14 -0800

    Completed Project

--- main.c
+++ main.c
@@ -4,7 +4,15 @@
  * Given code, Winter '24.
  * TODO: Complete this file.
  */
+#include "fib.h"
+#include <stdio.h>
 
-int main(void) {
-
+int main(int argc, char *argv[])
+{
+    int x, y;
+    printf("Enter an integer: ");
+    scanf("%d", &x);
+    y = fib(x);
+    printf("f(%d) = %d\n", x, y);
+    return 0;
 }
